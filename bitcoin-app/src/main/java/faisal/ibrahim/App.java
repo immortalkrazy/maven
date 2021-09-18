40bed584da38596e48039aeca2ee55fdad8a1fc5
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2021-09-17 23:02:46 -0400

    added some styuff

---
+++ bitcoin-app/src/main/java/faisal/ibrahim/App.java
@@ -0,0 +1,13 @@
+package faisal.ibrahim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
